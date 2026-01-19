342bf2f92cebe95ed65327e13a91b03829672948
hellicopthecat <jhccop@naver.com>
2026-01-19 22:23:31 +0900

    learn go 2

--- main.go
+++ main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"fmt"
 
-	"github.com/hellicopthecat/learngo/accounts"
+	"github.com/hellicopthecat/learngo/dict"
 )
 
 func main() {
-	account := accounts.NewAccount("choi")
-	account.Deposit(5)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
+	// account := accounts.NewAccount("choi")
+	// account.Deposit(5)
+	// fmt.Println(account.Balance())
+	// err := account.Withdraw(20)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// account.ChangeOwner("hohoho")
+	// fmt.Println(account.Balance(), account.GetOwner())
+	// fmt.Println(account)
+
+	myDict := dict.Dictionary{"first": "First word"}
+	// myDict["hoho"] = "haha"
+	// fmt.Println(myDict)
+	definition, err := myDict.Search("first")
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
 	}
-	account.ChangeOwner("hohoho")
-	fmt.Println(account.Balance(), account.GetOwner())
-	fmt.Println(account)
+	myDict.Add("hello", "definitions")
+	definition2, _ := myDict.Search("hello")
+	fmt.Println(definition2)
+	myDict.Update("hello", "This is say hello")
+	definition3, _ := myDict.Search("hello")
+	fmt.Println(definition3)
 }
